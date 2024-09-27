9ca2a41d818c683593c3848e18234da6c923493a
ImperaZim <imperaartz1@gmail.com>
2024-09-28 00:08:41 +0200

    ...

--- installer.sh
+++ installer.sh
@@ -1,9 +1,11 @@
+mkdir -p ./src/
 mkdir -p ./src/utils/
+
 cd ./src/utils/
 
 sudo apt-get install jq
 
-git clone "https://https://github.com/ImperaZim/EasyNTSUtils.git"
+git clone "https://github.com/ImperaZim/EasyNTSUtils.git"
 mv ./src/ ./ 
 
 cd ../../
@@ -12,4 +14,4 @@ if [ -f "./tsconfig.json" ]; then
 jq '.compilerOptions += { "baseUrl": "./" } | .compilerOptions.paths //= {} | .compilerOptions.paths += { "#utils": ["./src/utils/index.ts"], "#console": ["./src/utils/ConsoleUtils/index.ts"], "#json": ["./src/utils/JsonUtils/index.ts"], "#embed": ["./src/utils/EmbedUtils/index.ts"] }' tsconfig.json > tmp.json && mv tmp.json tsconfig.json
 fi 
 
-rm -rf ./installer.sh
+rm -rf ./installer.sh 

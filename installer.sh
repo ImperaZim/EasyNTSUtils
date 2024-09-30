6834cd001e7d6244c78937eb5ad4bef7f0ff1fcd
ImperaZim <imperaartz1@gmail.com>
2024-09-30 04:07:12 +0200

    ...

--- installer.sh
+++ installer.sh
@@ -1,24 +1,29 @@
-
+# Remove o diretório antigo @imperazim
 rm -rf @imperazim
 
+# Clona o repositório com a branch main
 git clone -b main https://github.com/ImperaZim/EasyNTSUtils.git
 
+# Cria o diretório @imperazim e move os arquivos para lá
 mkdir -p @imperazim
 mv EasyNTSUtils/src/* @imperazim/
 
-rm -rf EasyNTSUtijs
+# Remove o diretório EasyNTSUtils
+rm -rf EasyNTSUtils
 
+# Instala pacotes necessários
 npm i colorette fs --silent
 
+# Cria e executa o script para atualizar o tsconfig.json
 cat << 'EOF' > updateTsConfig.js
 const fs = require('fs');
-
 const filePath = './tsconfig.json';
 
 if (fs.existsSync(filePath)) {
+  // Lê o arquivo tsconfig.json
   const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
 
-  // Adiciona baseUrl
+  // Atualiza o tsconfig.json com as novas opções
   data.compilerOptions = {
     ...data.compilerOptions,
     "baseUrl": "./src/",
@@ -42,15 +47,23 @@ if (fs.existsSync(filePath)) {
       "DiscordModalComponent": ["DiscordElementor/modal/component"]
     }
   };
+
+  // Salva o arquivo atualizado com formatação
+  fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf8');
   console.log('Arquivo tsconfig.json atualizado!');
-}else{
+} else {
   console.log('Arquivo tsconfig.json não encontrado!');
 }
 EOF
 
+# Executa o script de atualização
 node updateTsConfig.js
+
+# Remove o script de atualização
 rm updateTsConfig.js
 
-rm -rf ./installer.sh 
+# Remove o arquivo installer.sh se existir
+rm -rf ./installer.sh
 
+# Mensagem de sucesso
 echo 'EasyNTSUtils Instalado!';

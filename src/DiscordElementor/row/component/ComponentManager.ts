cff82e24561dae51d76efcd37e6fa2ee6371c11d
ImperaZim <imperaartz1@gmail.com>
2024-10-01 00:57:35 +0200

    ...

--- src/DiscordElementor/row/component/ComponentManager.ts
+++ src/DiscordElementor/row/component/ComponentManager.ts
@@ -2,8 +2,10 @@ import {
   Row,
   Tags,
   getButtons,
-  BuilderRegistry, Builder,
-  getSelects, DiscordSelectTypes
+  BuilderRegistry,
+  Builder,
+  getSelects,
+  DiscordSelectTypes
 } from "../..";
 import { ActionRowBuilder, ButtonBuilder } from 'discord.js';
 
@@ -11,10 +13,10 @@ import { ActionRowBuilder, ButtonBuilder } from 'discord.js';
  * Obtém todos os componentes de uma linha específica.
  * @param row Nome da linha de onde os componentes devem ser obtidos.
  * @param tags Tags de replacement de mensagens.
- * @returns Um array de objetos ActionRowBuilder configurados.
+ * @returns Um array de objetos APIActionRowComponent configurados.
  * @throws Erros se a linha não for encontrada ou se não houver componentes.
  */
-export function getComponents(row: string, tags: Tags): ActionRowBuilder[] {
+export function getComponents(row: string, tags: Tags): readonly (APIActionRowComponent<APIButtonComponent> | APIActionRowComponent<APISelectComponent>)[] {
   const builder: Builder = BuilderRegistry.builder;
 
   if (!builder.rows) {
@@ -31,9 +33,20 @@ export function getComponents(row: string, tags: Tags): ActionRowBuilder[] {
     throw new Error(`Não foram encontrados componentes na lista ${row}!`);
   }
 
-  const rows: ActionRowBuilder[] = [];
-  rows.push(new ActionRowBuilder<ButtonBuilder>().addComponents(getButtons(row, tags)));
-  rows.push(new ActionRowBuilder<DiscordSelectTypes>().addComponents(getSelects(row, tags)));
+  const rows: ActionRowBuilder<ButtonBuilder | DiscordSelectTypes>[] = [];
 
-  return rows.map(component => component.toJSON()); // Retorna todos os ActionRowBuilder configurados
+  // Adiciona componentes de botões
+  const buttonComponents = getButtons(row, tags);
+  if (buttonComponents.length > 0) {
+    rows.push(new ActionRowBuilder<ButtonBuilder>().addComponents(buttonComponents));
+  }
+
+  // Adiciona componentes de seleção
+  const selectComponents = getSelects(row, tags);
+  if (selectComponents.length > 0) {
+    rows.push(new ActionRowBuilder<DiscordSelectTypes>().addComponents(selectComponents));
+  }
+
+  // Retorna os componentes em formato JSON
+  return rows.map(component => component.toJSON());
 }

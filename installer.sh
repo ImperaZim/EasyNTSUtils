35d30a1b65cd54091aed44ec430f76ea7a04dd75
Impera <imperaartz1@gmail.com>
2024-09-30 13:58:22 -0300

    update installer.sh

--- installer.sh
+++ installer.sh
@@ -14,51 +14,6 @@ rm -rf EasyNTSUtils
 # Instala pacotes necessários
 npm i colorette fs --silent
 
-# Cria e executa o script para atualizar o tsconfig.json
-cat << 'EOF' > updateTsConfig.js
-const fs = require('fs');
-const filePath = './tsconfig.json';
-
-if (fs.existsSync(filePath)) {
-  // Lê o arquivo tsconfig.json
-  const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-
-  // Atualiza o tsconfig.json com as novas opções
-  data.compilerOptions = {
-    ...data.compilerOptions,
-    "baseUrl": "./",
-    "paths": {
-      "Utils": ["./@imperazim/"],
-      "Console": ["./@imperazim/ConsoleUtils"],
-      "Json": ["./@imperazim/JsonUtils"],
-      "MySQL": ["./@imperazim/MySQLUtils"],
-      "DiscordElementor": ["./@imperazim/DiscordElementor"],
-      "DiscordElementCollector/*": ["./@imperazim/DiscordElementor/collector/*"],
-      "DiscordElementCollector": ["./@imperazim/DiscordElementor/collector"],
-      "DiscordRow": ["./@imperazim/DiscordElementor/row"],
-      "DiscordRowEmbed": ["./@imperazim/DiscordElementor/row/embed"],
-      "DiscordRowButton": ["./@imperazim/DiscordElementor/row/component/button"],
-      "DiscordRowSelect": ["./@imperazim/DiscordElementor/row/component/select"],
-      "DiscordModal": ["./@imperazim/DiscordElementor/modal"],
-      "DiscordModalInput": ["./@imperazim/DiscordElementor/modal/component/input"],
-      "DiscordModalComponent": ["./@imperazim/DiscordElementor/modal/component"]
-    }
-  };
-
-  // Salva o arquivo atualizado com formatação
-  fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf8');
-  console.log('Arquivo tsconfig.json atualizado!');
-} else {
-  console.log('Arquivo tsconfig.json não encontrado!');
-}
-EOF
-
-# Executa o script de atualização
-node updateTsConfig.js
-
-# Remove o script de atualização
-rm updateTsConfig.js
-
 # Remove o arquivo installer.sh se existir
 rm -rf ./installer.sh
 

e0a554f743a8092c1f5cd6bfaaca0e501e164852
ImperaZim <imperaartz1@gmail.com>
2024-09-30 03:42:09 +0200

    ...

--- installer.sh
+++ installer.sh
@@ -8,3 +8,51 @@ mv EasyNTSUtils/src/* @imperazim/
 
 rm -rf EasyNTSUtils
 
+if [ -f tsconfig.json ]; then 
+cat << 'EOF' > updateTsConfig.js
+const fs = require('fs');
+
+const filePath = './tsconfig.json';
+
+if (fs.existsSync(filePath)) {
+  const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
+  // Adiciona baseUrl
+  data.compilerOptions = {
+    ...data.compilerOptions,
+    "baseUrl": "./src/",
+    "paths": {
+      "Utils": ["./"],
+      "Console": ["./ConsoleUtils"],
+      "Json": ["./JsonUtils"],
+      "MySQL": ["./MySQLUtils"],
+      "DiscordElementor/*": ["DiscordElementor/*"],
+      "DiscordElementor": ["DiscordElementor"],
+      "DiscordElementCollector/*": ["DiscordElementor/collector/*"],
+      "DiscordElementCollector": ["DiscordElementor/collector"],
+      "DiscordRow/*": ["DiscordElementor/row/*"],
+      "DiscordRow": ["DiscordElementor/row"],
+      "DiscordRowEmbed": ["DiscordElementor/row/embed"],
+      "DiscordRowButton": ["DiscordElementor/row/component/button"],
+      "DiscordRowSelect": ["DiscordElementor/row/component/select"],
+      "DiscordModal/*": ["DiscordElementor/modal/*"],
+      "DiscordModal": ["DiscordElementor/modal"],
+      "DiscordModalInput": ["DiscordElementor/modal/component/input"],
+      "DiscordModalComponent": ["DiscordElementor/modal/component"]
+    }
+  };
+}
+EOF
+
+node updateTsConfig.js
+
+rm updateTsConfig.js 
+else
+echo 'Arquivo tsconfig não encontrado!';
+fi;
+
+npm i colorette
+
+rm -rf ./installer.sh 
+
+echo 'EasyNTSUtils Instalado!';

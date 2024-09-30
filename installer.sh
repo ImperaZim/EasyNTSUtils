d7ab4f4ec2b304d20917dbaf604799cd5446b12e
ImperaZim <imperaartz1@gmail.com>
2024-09-30 03:32:23 +0200

    ...

--- installer.sh
+++ installer.sh
@@ -1,6 +1,6 @@
 
 rm -rf ./*
-git clone -b main https://github.com/ImperaZim/EasyNTSUtils.git 
+git clone -b main https://github.com/ImperaZim/EasyNTSUtils.git --silent
 
 mkdir -p @imperazim
 mv EasyNTSUtils/src/* @imperazim/

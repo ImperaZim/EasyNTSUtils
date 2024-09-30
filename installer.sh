fb8efdf172761dcfd287a3ad818d5ffabd701984
Impera <imperaartz1@gmail.com>
2024-09-30 14:06:24 -0300

    update installer.sh

--- installer.sh
+++ installer.sh
@@ -14,53 +14,10 @@ rm -rf EasyNTSUtils
 # Instala pacotes necessários
 npm i colorette fs --silent
 
-# Cria e executa o script para atualizar o tsconfig.json
-cat << 'EOF' > updateTsConfig.js
-const fs = require('fs');
-const filePath = './tsconfig.json';
+rm tsconfig.json 
 
-if (fs.existsSync(filePath)) {
-  // Lê o arquivo tsconfig.json
-  const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+cat << 'EOF' > updateTsConfig.json
 
-  // Atualiza o tsconfig.json com as novas opções
-  data.compilerOptions = {
-    ...data.compilerOptions,
-    "baseUrl": "./@imperazim/",
-    "paths": {
-      "Utils": ["./"],
-      "Console": ["./ConsoleUtils"],
-      "Json": ["./JsonUtils"],
-      "MySQL": ["./MySQLUtils"],
-      "DiscordElementor/*": ["DiscordElementor/*"],
-      "DiscordElementor": ["DiscordElementor"],
-      "DiscordElementCollector/*": ["DiscordElementor/collector/*"],
-      "DiscordElementCollector": ["DiscordElementor/collector"],
-      "DiscordRow/*": ["DiscordElementor/row/*"],
-      "DiscordRow": ["DiscordElementor/row"],
-      "DiscordRowEmbed": ["DiscordElementor/row/embed"],
-      "DiscordRowButton": ["DiscordElementor/row/component/button"],
-      "DiscordRowSelect": ["DiscordElementor/row/component/select"],
-      "DiscordModal/*": ["DiscordElementor/modal/*"],
-      "DiscordModal": ["DiscordElementor/modal"],
-      "DiscordModalInput": ["DiscordElementor/modal/component/input"],
-      "DiscordModalComponent": ["DiscordElementor/modal/component"]
-    }
-  };
-
-  // Salva o arquivo atualizado com formatação
-  fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf8');
-  console.log('Arquivo tsconfig.json atualizado!');
-} else {
-  console.log('Arquivo tsconfig.json não encontrado!');
-}
-EOF
-
-# Executa o script de atualização
-node updateTsConfig.js
-
-# Remove o script de atualização
-rm updateTsConfig.js
 
 # Remove o arquivo installer.sh se existir
 rm -rf ./installer.sh

42be03ece9e0f9b5f38a6e85a062dbe3e98df152
Impera <imperaartz1@gmail.com>
2024-09-30 21:18:02 -0300

    update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,35 +1,25 @@
 #!/bin/bash
 
-# Função para mostrar animação de loading com mensagem
-loading() {
-    local pid=$1
-    local message=$2
-    local delay=0.1
-    local spin='/-\|'
-    local i=0
-
-    echo -ne "\r$message"
-}
-
 # Remove o diretório antigo @imperazim
 rm -rf @imperazim &
-loading $! "Removendo @imperazim"
 
 # Clona o repositório com a branch main de forma silenciosa
 git clone -b main https://github.com/ImperaZim/EasyNTSUtils.git > /dev/null 2>&1 &
-loading $! "Baixando Dados"
+echo "Baixando Dados"
 
 # Cria o diretório @imperazim e move os arquivos para lá
 mkdir -p @imperazim
 mv EasyNTSUtils/src/* @imperazim/
 mv EasyNTSUtils/tsconfig.json ./tsconfig.json
+echo "Compilando Dados"
 
 # Remove o diretório EasyNTSUtils
 rm -rf EasyNTSUtils &
+echo "Removendo vestigios"
 
 # Instala pacotes necessários de forma silenciosa
 npm i colorette fs --silent &
-loading $! "Instalando Pacotes"
+echo "Instalando Pacotes"
 
 # Remove o arquivo installer.sh se existir
 rm -rf ./installer.sh

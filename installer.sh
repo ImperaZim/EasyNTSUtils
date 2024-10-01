08036161388c67a8e7ab11e886bb31e401ad70a7
Impera <imperaartz1@gmail.com>
2024-09-30 21:35:52 -0300

    update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,9 +1,9 @@
 # Remove o diretório antigo @imperazim
-echo 'Limpandos versões anterioes!';
+echo -e '\e[1;33m[imperazim/utils]\e[0m \e[1;37mLimpandos versões anterioes!';
 rm -rf @imperazim
 
 # Clona o repositório com a branch main
-echo 'Baixando sub-modulos:'
+echo -e '\e[1;33m[imperazim/utils]\e[0m \e[1;37mBaixando sub-modulos:'
 echo "- DiscordElmodulos"
 echo "- JsonUtils"
 echo "- ConsoleUtils"
@@ -11,26 +11,26 @@ echo "- MySQLUtils"
 git clone -b main https://github.com/ImperaZim/EasyNTSUtils.git --quiet
 
 # Cria o diretório @imperazim e move os arquivos para lá
-echo 'Criando modulo @imperazim!'
+echo -e '\e[1;33m[imperazim/utils]\e[0m \e[1;37mCriando modulo @imperazim!'
 mkdir -p @imperazim 
 
-echo 'Instalando sub-modulos em @imperazim!'
+echo -e '\e[1;33m[imperazim/utils]\e[0m \e[1;37mInstalando sub-modulos em @imperazim!'
 mv EasyNTSUtils/src/* @imperazim/
 
-echo 'Criando compilador tsconfig!'
+echo -e '\e[1;33m[imperazim/utils]\e[0m \e[1;37mCriando compilador tsconfig!'
 mv EasyNTSUtils/tsconfig.json ./tsconfig.json
 
 # Remove o diretório EasyNTSUtils
-echo 'Removendo vestigios!'
+echo -e '\e[1;33m[imperazim/utils]\e[0m \e[1;37mRemovendo vestigios!'
 rm -rf EasyNTSUtils
 
 # Instala pacotes necessários
-echo 'Instalando pacotes necessários!'
+echo -e '\e[1;33m[imperazim/utils]\e[0m \e[1;37mInstalando pacotes necessários!'
 npm i colorette fs --silent
 
 # Remove o arquivo installer.sh se existir
-echo 'Removendo instalador!'
+echo -e '\e[1;33m[imperazim/utils]\e[0m \e[1;37mRemovendo instalador!'
 rm -rf ./installer.sh
 
 # Mensagem de sucesso
-echo 'EasyNTSUtils Instalado!';
+echo -e '\e[1;33m[imperazim/utils]\e[0m \e[1;37mEasyNTSUtils Instalado!';

b792a0d764bbb6e42e002aec5878993373b90cac
Impera <imperaartz1@gmail.com>
2024-09-30 21:43:29 -0300

    update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
-echo -e "\e[1;33m[imperazim]\e[0m \e[1;37mLimpandos versões anterioes!";
+echo -e "\e[1;33m[imperazim]:\e[0m \e[1;37mLimpandos versões anterioes!";
 rm -rf @imperazim
 
-echo -e "\e[1;33m[imperazim]\e[0m \e[1;37mBaixando sub-modulos:"
-echo -e "\e[1;33m[imperazim/utils]\e[0m \e[1;37mDiscordElementor Instalado"
-echo -e "\e[1;33m[imperazim/utils]\e[0m \e[1;37mJsonUtils Instalado"
-echo -e "\e[1;33m[imperazim/utils]\e[0m \e[1;37mConsoleUtils Instalado"
-echo -e "\e[1;33m[imperazim/utils]\e[0m \e[1;37mMySQLUtils Instalado"
+echo -e "\e[1;33m[imperazim]:\e[0m \e[1;37mBaixando sub-modulos:"
+echo -e "\e[1;33m[imperazim/utils]:\e[0m \e[1;37mDiscordElementor Instalado"
+echo -e "\e[1;33m[imperazim/utils]:\e[0m \e[1;37mJsonUtils Instalado"
+echo -e "\e[1;33m[imperazim/utils]:\e[0m \e[1;37mConsoleUtils Instalado"
+echo -e "\e[1;33m[imperazim/utils]:\e[0m \e[1;37mMySQLUtils Instalado"
 git clone -b main https://github.com/ImperaZim/EasyNTSUtils.git --quiet
 
-echo -e "\e[1;33m[imperazim]\e[0m \e[1;37mCriando modulo @imperazim!"
+echo -e "\e[1;33m[imperazim]:\e[0m \e[1;37mCriando modulo @imperazim!"
 mkdir -p @imperazim 
 
-echo -e "\e[1;33m[imperazim]\e[0m \e[1;37mInstalando sub-modulos em @imperazim!"
+echo -e "\e[1;33m[imperazim]:\e[0m \e[1;37mInstalando sub-modulos em @imperazim!"
 mv EasyNTSUtils/src/* @imperazim/
 
-echo -e "\e[1;33m[imperazim]\e[0m \e[1;37mCriando compilador tsconfig!"
+echo -e "\e[1;33m[imperazim]:\e[0m \e[1;37mCriando compilador tsconfig!"
 mv EasyNTSUtils/tsconfig.json ./tsconfig.json
 
 # Remove o diretório EasyNTSUtils
-echo -e "\e[1;33m[imperazim]\e[0m \e[1;37mRemovendo vestigios!"
+echo -e "\e[1;33m[imperazim]:\e[0m \e[1;37mRemovendo vestigios!"
 rm -rf EasyNTSUtils
 
 # Instala pacotes necessários
-echo -e "\e[1;33m[imperazim]\e[0m \e[1;37mInstalando pacotes necessários!"
+echo -e "\e[1;33m[imperazim]:\e[0m \e[1;37mInstalando pacotes necessários!"
 npm i colorette fs --silent
 
 # Remove o arquivo installer.sh se existir
-echo -e "\e[1;33m[imperazim]\e[0m \e[1;37mRemovendo instalador!"
+echo -e "\e[1;33m[imperazim]:\e[0m \e[1;37mRemovendo instalador!"
 rm -rf ./installer.sh
 
 # Mensagem de sucesso
-echo -e "\e[1;33m[imperazim]\e[0m \e[1;37mEasyNTSUtils Instalado!";
+echo -e "\e[1;33m[imperazim]:\e[0m \e[1;37mEasyNTSUtils Instalado!";

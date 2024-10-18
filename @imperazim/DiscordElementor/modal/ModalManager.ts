42dab241b159501023b981b3907c2ec76020df27
Impera <imperaartz1@gmail.com>
2024-10-18 15:38:48 -0300

    update @imperazim/DiscordElementor/modal/ModalManager.ts

--- @imperazim/DiscordElementor/modal/ModalManager.ts
+++ @imperazim/DiscordElementor/modal/ModalManager.ts
@@ -28,8 +28,6 @@ export function getModal(modal: string, tags: Tags): ModalBuilder {
   const modalData: Modal = builder.modals[modal];
   const components = getModalComponents(modal, tags);
   
-  console.log(components);
-
   const modalBuilder = new ModalBuilder();
   modalBuilder.setCustomId(modal);
   modalData.title && modalBuilder.setTitle(modalData.title);

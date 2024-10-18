8a3ae46600fbea8f5cf2882c402759f5ab5d7ce7
Impera <imperaartz1@gmail.com>
2024-10-17 23:06:10 -0300

    update @imperazim/DiscordElementor/collector/GlobalCollector.ts

--- @imperazim/DiscordElementor/collector/GlobalCollector.ts
+++ @imperazim/DiscordElementor/collector/GlobalCollector.ts
@@ -10,40 +10,45 @@ import {
   MentionableSelectMenuInteraction,
 } from "discord.js";
 
+interface GlobalCollectorOptions {
+  response: InteractionResponse | any;
+  componentType: ComponentType;
+  timeout: number;
+  callback: (
+    interaction:
+      | ButtonInteraction
+      | SelectMenuInteraction
+      | ModalSubmitInteraction
+      | RoleSelectMenuInteraction
+      | UserSelectMenuInteraction
+      | ChannelSelectMenuInteraction
+      | MentionableSelectMenuInteraction
+      | any
+  ) => void;
+  filter?: (
+    interaction:
+      | ButtonInteraction
+      | SelectMenuInteraction
+      | ModalSubmitInteraction
+      | RoleSelectMenuInteraction
+      | UserSelectMenuInteraction
+      | ChannelSelectMenuInteraction
+      | MentionableSelectMenuInteraction
+      | any
+  ) => boolean;
+}
+
 export class GlobalCollector {
-  constructor(
-    response: InteractionResponse | any,
-    componentType: ComponentType,
-    timeout: number,
-    callback: (
-      interaction:
-        | ButtonInteraction
-        | SelectMenuInteraction
-        | ModalSubmitInteraction
-        | RoleSelectMenuInteraction
-        | UserSelectMenuInteraction
-        | ChannelSelectMenuInteraction
-        | MentionableSelectMenuInteraction
-        | any
-    ) => void,
-    filter?: (
-      interaction:
-        | ButtonInteraction
-        | SelectMenuInteraction
-        | ModalSubmitInteraction
-        | RoleSelectMenuInteraction
-        | UserSelectMenuInteraction
-        | ChannelSelectMenuInteraction
-        | MentionableSelectMenuInteraction
-        | any
-    ) => boolean
-  ) {
+  constructor(options: GlobalCollectorOptions) {
+    const { response, componentType, timeout, callback, filter } = options;
+
     if (response) {
       const collector = response.createMessageComponentCollector({
         time: timeout,
         componentType: componentType,
         filter: filter,
       });
+
       if (collector) {
         collector.on("collect", callback);
       }

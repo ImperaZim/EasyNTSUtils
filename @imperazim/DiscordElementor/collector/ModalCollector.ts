ff2854124bfab306e3fced04f4fe572b83815192
Impera <imperaartz1@gmail.com>
2024-10-18 00:09:12 -0300

    update @imperazim/DiscordElementor/collector/ModalCollector.ts

--- @imperazim/DiscordElementor/collector/ModalCollector.ts
+++ @imperazim/DiscordElementor/collector/ModalCollector.ts
@@ -10,27 +10,21 @@ interface ModalCollectorOptions {
   filter?: (interaction: ModalSubmitInteraction) => boolean;
 }
 
-/**
- * Coletor de interações de modals.
- */
 export class ModalCollector {
   constructor(options: ModalCollectorOptions) {
     const { response, timeout, callback, filter } = options;
 
     if (response && response.awaitModalSubmit) {
-      // Chama awaitModalSubmit e lida com a promessa
       response.awaitModalSubmit({
         time: timeout,
         filter: filter || ((interaction: ModalSubmitInteraction) => interaction.isModalSubmit()),
       })
       .then(interaction => {
-        // Chama o callback quando a interação é coletada
         if (interaction) {
           callback(interaction);
         }
       })
       .catch(error => {
-        // Lide com o erro aqui, se necessário
         console.error('Erro ao coletar a interação de modal:', error);
       });
     }
